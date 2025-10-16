b845696c32ef4f699fa5645ab0c3cb87325fa785
Sasha Cattaneo <s.cattaneo29@studenti.unibg.it>
2025-10-16 21:36:39 +0200

    Changed name for MusicScript to MusicManager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
